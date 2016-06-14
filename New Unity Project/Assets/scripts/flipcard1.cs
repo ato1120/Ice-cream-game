6474025a6a1e4613a2a354bd60313124be275aaf
JiaqiWang319 <jqw319@gmail.com>
2016-06-14 12:09:32 +0800

    Initial commit

---
+++ New Unity Project/Assets/scripts/flipcard1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipcard1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public float roate_Speed = 100.0f;//旋转速度  
+void Update()
+{  
+Transform target_transform = null;//不用绑定对象，下面代码动态获取对象  
+if (Input.GetMouseButton(0))  
+            {                   
+                //在屏幕上转换坐标：将鼠标点转换成射线  
+                Ray rayObj = Camera.main.ScreenPointToRay(Input.mousePosition);  
+                RaycastHit hitObj;  
+                if (Physics.Raycast(rayObj, out hitObj))  
+                {  
+                    //Debug.Log("射线得到的对象名称：" + hitObj.collider.name);  
+                    target_transform = hitObj.transform;  
+                }  
+  
+  
+                if (target_transform != null)  
+                {  
+                    //Debug.Log("射线取得对象");  
+                    float mousX = Input.GetAxis("Mouse X") * roate_Speed;//得到鼠标移动距离  
+                    target_transform.transform.Rotate(new Vector3(0, -mousX, 0));  
+                }  
+                else  
+                {  
+                    Debug.Log("无法取得对象");  
+                }  
+            }  
+}  
+
+}
---
+++ New Unity Project/Assets/scripts/flipcard2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipcard2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    float speed = 100.0f;
+    float x;
+    float y;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {//鼠标按着左键移动 
+            y = Input.GetAxis("Mouse X") * Time.deltaTime * speed;
+            x = Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
+        }
+        else
+        {
+            x = y = 0;
+        }
+
+        //旋转角度（增加）
+        transform.Rotate(new Vector3(x, y, 0));
+        /**---------------其它旋转方式----------------**/
+        //transform.Rotate(Vector3.up *Time.deltaTime * speed);//绕Y轴 旋转 
+
+    }
+}
