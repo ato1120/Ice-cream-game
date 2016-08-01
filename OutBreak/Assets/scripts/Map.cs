a981d83b29ced4ab59998fa9dc6449cf745280b6
wangjiaqi <jqw319@gmail.com>
2016-08-01 16:50:49 -0400

    Data structure of OutBreak

---
+++ OutBreak/Assets/scripts/Map.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapCard : MonoBehaviour
+{
+
+    public string title;
+    public Object mapImg;
+    public string fulldescription;
+    public int id;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+}
+
+public class Map : MonoBehaviour {
+
+    MapCard map1;
+    // Use this for initialization
+    void Start () {
+
+        map1.title = "Biologglab";
+        map1.mapImg = GameObject.Find("map1Img");
+        map1.fulldescription = "good";
+        map1.id = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+   
+    }
+}
---
+++ OutBreak/Assets/scripts/MapCard.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map1 : MonoBehaviour
+{
+
+    public string title;
+    public Object mapImg;
+    public string fulldescription;
+    public int id;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    }
+   
+}
+public class Map2 : MonoBehaviour
+{
+
+    public string title;
+    public Object mapImg;
+    public string fulldescription;
+    public int id;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+}
